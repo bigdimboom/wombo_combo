fa5ea4066e7431976c06cd72cc89822c5d7d6d8f
LiangLUO <lluo1@stevens.edu>
2015-02-10 00:43:51 -0500

    Window Creation in SDL

---
+++ wombo_combo/wombo_combo/Main.cpp
@@ -0,0 +1,24 @@
+
+#include <SDL.h>
+
+int main(int argc, char** argv){
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 512, 512, SDL_WINDOW_SHOWN);
+
+	SDL_Event ev;
+	bool isRunning = true;
+	while (isRunning){
+		SDL_PollEvent(&ev);
+		if (ev.type == SDL_QUIT){
+			isRunning = false;
+		}
+	}
+
+	SDL_DestroyWindow(window);
+
+	SDL_Quit();
+
+	return 0;
+}
