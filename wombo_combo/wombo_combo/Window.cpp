65781b6dc9050d833987eac81a78e4ce5d694b87
LiangLUO <lluo1@stevens.edu>
2015-02-17 22:35:45 -0500

    Create the Window Class

---
+++ wombo_combo/wombo_combo/Window.cpp
@@ -0,0 +1,105 @@
+#include "Window.h"
+
+Window::Window(int width, int height, const char* title)
+	:_title(title), _width(width), _height(height),
+	_isClose(true), _pWindow(nullptr), _isVSyncOn(false)
+{
+	assert(IS_SYSTEM_INIT != false);
+}
+
+
+Window::~Window()
+{
+}
+
+bool Window::Open()
+{
+	// Create an application window with the following settings:
+	_pWindow = SDL_CreateWindow(
+		_title.c_str(),                  // window title
+		SDL_WINDOWPOS_UNDEFINED,         // initial x position
+		SDL_WINDOWPOS_UNDEFINED,         // initial y position
+		WINDOW_WIDTH,                    // width, in pixels
+		WINDOW_HEIGHT,                   // height, in pixels
+		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+		// flags - see below
+		);
+
+	// Check that the window was successfully made
+	if (_pWindow == nullptr) {
+		// In the event that the window could not be made...
+		printf("Could not create window: %s\n", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void Window::Close()
+{
+	_isClose = true;
+	SDL_DestroyWindow(_pWindow);
+}
+
+bool Window::InitGL()
+{
+	assert(_pWindow != nullptr);
+	_glContext = SDL_GL_CreateContext(_pWindow);
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	SDL_GL_MakeCurrent(_pWindow, _glContext);
+
+	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+
+	glewExperimental = GL_TRUE;
+
+	GLenum glew_status = glewInit();
+
+	if (glew_status != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(glew_status));
+		return false;
+	}
+
+#ifdef DEBUG
+	printf("OpenGL (%s): \n", glGetString(GL_VERSION));
+	printf("GLSL: (%s): \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+	printf("GPU: (%s %s): \n\n", glGetString(GL_RENDERER), glGetString(GL_VENDOR));
+#endif
+
+#ifdef DEBUG
+	SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+#endif
+
+	if (_isVSyncOn)
+	{
+		SDL_GL_SetSwapInterval(1);
+	}
+	else
+	{
+		SDL_GL_SetSwapInterval(0);
+	}
+
+	return true;
+}
+
+void Window::DestoryGL()
+{
+	assert(_glContext != nullptr);
+	SDL_GL_DeleteContext(_glContext);
+}
+
+void Window::SwapBuffers()
+{
+	assert(_glContext != nullptr);
+	SDL_GL_SwapWindow(_pWindow);
+}
